ad0b38122f55c27b27790d3543e84631fc0d7982
Daniel Toader & Kyle Bulloch <student@epicodus.com>
2015-04-30 09:59:33 -0700

    add timeout for first blink

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -19,22 +19,17 @@ simon.controller("GameCtrl", function($scope, $timeout) {
     $scope.score = 0;
     addSimon();
     flashSimon();
-  }
+  };
 
   $scope.click = function(btn) {
     user_pattern.push(btn);
-    console.log(user_pattern);
     user_pattern.length === simon_pattern.length ? match() : "";
   };
 
   function match() {
     for (var i in user_pattern) {
       if (user_pattern[i] != simon_pattern[i]) {
-        console.log("fail");
-        console.log("i = " + i);
-        console.log("upl = " + user_pattern.length);
         if (i == user_pattern.length - 1) {
-          console.log("i am in you ;)");
           user_pattern = [];
           simon_pattern = [];
           $scope.fail = true;
@@ -55,26 +50,31 @@ simon.controller("GameCtrl", function($scope, $timeout) {
   function addSimon() {
     var btns = ["btn1", "btn2", "btn3", "btn4"];
     simon_pattern.push(btns[Math.floor(Math.random() * btns.length)]);
-    console.log("simon: " + simon_pattern);
   }
 
   function  flashSimon() {
-    for (var i in simon_pattern) {
-      (function(i) {
-        $timeout(function() {
-          console.log("now on " + simon_pattern[i]);
-          $scope[simon_pattern[i]] = simon_pattern[i] + "b";
-          console.log("transformed to " + $scope[simon_pattern[i]]);
-        }, 700 * i);
-      })(i);
+    (function() {
+      $timeout(function() {
+        for (var i in simon_pattern) {
+          (function(i) {
+            $timeout(function() {
+              $scope[simon_pattern[i]] = simon_pattern[i] + "b";
+            }, 700 * i);
+          })(i);
 
-      (function(i) {
-        $timeout(function() {
-          $scope[simon_pattern[i]] = simon_pattern[i];
-          console.log("turned back to " + $scope[simon_pattern[i]]);
-        }, 70 * (i + 5));
-      })(i);
-    }
+          (function(i) {
+            $timeout(function() {
+              $scope[simon_pattern[i]] = simon_pattern[i];
+            }, 70 * (i + 5));
+          })(i);
+
+          (function(i) {
+            $timeout(function() {
+            }, 7 * (i + 10));
+          })(i);
+        }
+      }, 700);
+    })();
   }
 
 });
